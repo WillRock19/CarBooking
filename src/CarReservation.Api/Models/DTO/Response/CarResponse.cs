221302351abac4b186286f2edde201b968b6f803
William Porto <william@nfe.io>
2023-06-25 20:54:42 -0300

    Refactored

--- src/CarReservation.Api/Models/DTO/Response/CarResponse.cs
+++ src/CarReservation.Api/Models/DTO/Response/CarResponse.cs
@@ -2,8 +2,8 @@
 {
     public class CarResponse
     {
-        public string Id { get; set; } = "";
-        public string Make { get; set; } = "";
-        public string Model { get; set; } = "";
+        public required string Id { get; set; }
+        public required string Make { get; set; }
+        public required string Model { get; set; }
     }
 }
--- test/CarReservation.Api.Tests.Integration/Config/GlobalSetup.cs
+++ test/CarReservation.Api.Tests.Integration/Config/GlobalSetup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Hosting;
+using NUnit.Framework;
 
 namespace CarReservation.Api.Tests.Integration.Config
 {
